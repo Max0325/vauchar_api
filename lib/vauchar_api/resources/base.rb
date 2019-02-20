c3931683f2f432253633e225f31350d1f200714e
Max <max.liu0325@gmail.com>
2019-02-20 15:03:23 +0800

    f

--- lib/vauchar_api/resources/base.rb
+++ lib/vauchar_api/resources/base.rb
@@ -1,16 +1,19 @@
 require "vauchar_api/version"
 
-require "json_formatter"
-
 module VaucharAPI
   class Base < ActiveResource::Base
     cattr_accessor :static_headers
     self.static_headers = headers
     self.site = "https://api.vauchar.com"
-    self.include_format_in_path = false
-    self.include_root_in_json = false
+    # self.include_format_in_path = false
+    # self.include_root_in_json = false
 
     class << self
+      def element_path(id, prefix_options = {}, query_options = nil)
+        prefix_options, query_options = split_options(prefix_options) if query_options.nil?
+        "#{prefix(prefix_options)}#{collection_name}/#{URI.parser.escape id.to_s}#{query_string(query_options)}"
+      end
+
       # def user
       #   Configuration.merchant_id
       # end
