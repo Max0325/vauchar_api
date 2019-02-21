10e6096d5a014a822572d382cee399d26487d9f9
Max <max.liu0325@gmail.com>
2019-02-21 10:46:25 +0800

    f

--- lib/active_resource/formats_ext.rb
+++ lib/active_resource/formats_ext.rb
@@ -1,6 +1,9 @@
 module ActiveResource
   module Formats
     def self.remove_root(data)
+      data = data.with_indifferent_access
+      data = data.key?(:data) ? data[:data] : [data]
+
       if data.is_a?(Hash) && data.keys.size == 1 && data.values.first.is_a?(Enumerable)
         data.values.first
       else
