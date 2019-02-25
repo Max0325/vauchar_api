0b42d950dd5479a0de2223201126a83ac2064bc5
Max <max.liu0325@gmail.com>
2019-02-25 17:28:05 +0800

    fix

--- lib/vauchar_api.rb
+++ lib/vauchar_api.rb
@@ -17,14 +17,3 @@ require "vauchar_api/resources"
 require "vauchar_api/connection"
 require "active_resource/connection_ext"
 require "active_resource/custom_methods_ext"
-
-# ActiveSupport::Notifications.subscribe("request.active_resource") do |name, start, finish, id, payload|
-#   p payload
-# end
-
-VaucharAPI.config do |config|
-  config.merchant_id = "da2c250b-c417-42d8-936e-9bc4c246f90e"
-  config.api_key = "8d12f7f6c8a8fe505fc20b2cee6c71fc"
-end
-
-# require 'vauchar_api'
