6637eec66c3b66ec857606af9f4ca7629493c313
Max <max.liu0325@gmail.com>
2019-03-25 11:47:28 +0800

    fix

--- lib/vauchar_api/resources/deal_redemption.rb
+++ lib/vauchar_api/resources/deal_redemption.rb
@@ -7,7 +7,7 @@ module VaucharAPI
   #
   #   Read
   #   list = VaucharAPI::List.find("list-15508273755c6fbf6fc8ac7")
-  #   redemptions = VaucharAPI::Redemption.all
+  #   redemptions = VaucharAPI::DealRedemption.all
   #
   #   Update
   #   list = VaucharAPI::List.find("list-15508273755c6fbf6fc8ac7")
@@ -33,11 +33,19 @@ module VaucharAPI
   #   Find By Brand
   #   lists = VaucharAPI::List.find(:all, params: {brand_id: "brn-15507182295c6e1515a956a"})
 
-  class Redemption < Base
-    self.prefix = "/engage/deals/"
+  class DealRedemption < Base
+    self.prefix = "/engage/"
+    self.element_name = "deals/redemptions"
+
+    def redemption_id
+      # self.prefix = ""
+      resource = get("vouchers/#{self.voucher_id}/redemptions", {})
+      self.class.format.decode(resource.body)
+    end
 
     def destroy
-      # update_attribute(:status, 0)
+      resource = delete("vouchers/#{self.voucher_id}/redemptions/#{}", {}, only_id)
+      self.class.format.decode(resource.body)
     end
   end
 end
