338ec340f1bb8135d309a84a54f25140424ea8fb
Max <max.liu0325@gmail.com>
2019-03-27 17:42:17 +0800

    fix

--- lib/vauchar_api/resources/list.rb
+++ lib/vauchar_api/resources/list.rb
@@ -6,11 +6,11 @@ module VaucharAPI
   #   new_list.save
   #
   #   Read
-  #   list = VaucharAPI::List.find("list-15508273755c6fbf6fc8ac7")
+  #   list = VaucharAPI::List.find("list-15536636555c9b06a77c306")
   #   lists = VaucharAPI::List.all
   #
   #   Update
-  #   list = VaucharAPI::List.find("list-15508273755c6fbf6fc8ac7")
+  #   list = VaucharAPI::List.find("list-15536763035c9b380f9b7cb")
   #   list.name = "Max update"
   #   list.save
   #
@@ -20,7 +20,7 @@ module VaucharAPI
   #
   #   Customers
   #   list = VaucharAPI::List.find("list-15502036875c663b27600fe")
-  #   list.get(:customers);
+  #   list.get(:customers)
   #
   #   Add Customers
   #   list = VaucharAPI::List.find("list-15502036875c663b27600fe")
@@ -28,7 +28,7 @@ module VaucharAPI
   #
   #   Delete Customers
   #   list = VaucharAPI::List.find("list-15502036875c663b27600fe")
-  #   list.delete(:customers, {}, {customers: ["cust-15508280955c6fc23f7f807", "cust-15507323535c6e4c416c697"]})
+  #   list.delete(:customers, {}, {customers: ["cust-15536636405c9b0698e24a5"]})
   #
   #   Find By Brand
   #   lists = VaucharAPI::List.find(:all, params: {brand_id: "brn-15507182295c6e1515a956a"})
