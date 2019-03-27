2922838cff7b2b10e6c45a2e7827e0bf8dbca431
Max <max.liu0325@gmail.com>
2019-03-27 18:36:17 +0800

    fix

--- lib/vauchar_api.rb
+++ lib/vauchar_api.rb
@@ -4,13 +4,13 @@ require "vauchar_api/version"
 require "active_resource"
 require "active_resource/formats_ext"
 
-require "vauchar_api/resources"
-require "vauchar_api/connection"
-require "active_resource/connection_ext"
-require "active_resource/custom_methods_ext"
-
 module VaucharAPI
   def self.config
     yield Configuration
   end
 end
+
+require "vauchar_api/resources"
+require "vauchar_api/connection"
+require "active_resource/connection_ext"
+require "active_resource/custom_methods_ext"
