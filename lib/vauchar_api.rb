f1ac3313f1200b90eb2d6807c89d994d6884506c
Max <max.liu0325@gmail.com>
2019-03-27 22:11:05 +0800

    fix

--- lib/vauchar_api.rb
+++ lib/vauchar_api.rb
@@ -1,8 +1,8 @@
-require "active_resource"
-require "active_resource/formats_ext"
-
 require "vauchar_api/configuration"
 require "vauchar_api/version"
+
+require "active_resource"
+require "active_resource/formats_ext"
 
 module VaucharAPI
   def self.config
@@ -12,16 +12,11 @@ end
 
 require "vauchar_api/resources"
 require "vauchar_api/connection"
-
-p "VaucharAPI::Base.respond_to?(:connection_class)"
-p VaucharAPI::Base.respond_to?(:connection_class)
+require "active_resource/connection_ext"
+# require "active_resource/custom_methods_ext"
 
 if VaucharAPI::Base.respond_to?(:connection_class)
   VaucharAPI::Base.connection_class = VaucharAPI::Connection
 else
   require 'active_resource/connection_ext'
 end
-
-t = VaucharAPI::Connection.new("https://api.vauchar.com", {})
-p t.methods
-p t.methods.grep(/delete/)
--- lib/vauchar_api/version.rb
+++ lib/vauchar_api/version.rb
@@ -1,3 +1,3 @@
 module VaucharAPI
-  VERSION = "0.3.7"
+  VERSION = "0.3.8"
 end
